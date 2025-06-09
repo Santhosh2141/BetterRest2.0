b2132b19385dd4b36968a2d002482a5880c230ea
Santhosh2141 <snthshsrinivas@gmail.com>
2025-06-09 20:27:34 +0530

    first commit

---
+++ Better Rest 2.0/Better_Rest_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Better_Rest_2_0App.swift
+//  Better Rest 2.0
+//
+//  Created by Santhosh Srinivas on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Better_Rest_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Better Rest 2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Better Rest 2.0
+//
+//  Created by Santhosh Srinivas on 09/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
